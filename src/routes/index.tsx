6cb598f5d6d29bd89b0b3b6c923a70def956080d
Joao <joaovictorcordeiro00@gmail.com>
2024-01-22 19:27:25 -0300

    fazendo teste básico de componente do MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const RotasDoApp = () => {
 
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página Inicial</p>}/>
+            <Route path="/pagina-inicial" element={<Button>Testeee</Button>}/>
             <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
         </Routes>
     )
