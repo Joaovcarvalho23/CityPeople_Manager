66ae087ba9bd27fc114e9760c89ed95ecdc88631
Joao <joaovictorcordeiro00@gmail.com>
2024-01-22 19:23:55 -0300

    Definindo as rotas de teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { RotasDoApp } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-      Testeeee
-    </div>
+    <BrowserRouter>
+      <RotasDoApp />
+    </BrowserRouter>
   );
 }
