35b794ae5a9a84a04b50e53d9f0781f460d55a5d
Joao <joaovictorcordeiro00@gmail.com>
2024-01-23 19:30:54 -0300

    adaptando arquivos com as novas regras do ESLint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,18 @@
-import { Button } from "@mui/material";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts";
+/* eslint-disable react/react-in-jsx-scope */
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const RotasDoApp = () => {
-    const { toggleTheme } = useAppThemeContext();
-    return(
-        <Routes>
-            <Route 
-                path="/pagina-inicial" 
-                element={<Button variant="contained" color="primary" onClick={toggleTheme}>Toggle Theme!</Button>}/>
+  const { toggleTheme } = useAppThemeContext();
+  return(
+    <Routes>
+      <Route 
+        path="/pagina-inicial" 
+        element={<Button variant="contained" color="primary" onClick={toggleTheme}>Toggle Theme!</Button>}/>
            
-            <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
-        </Routes>
-    )
+      <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
+    </Routes>
+  );
 
- };
+};
