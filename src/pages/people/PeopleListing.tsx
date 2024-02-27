592b344473946aef944a6067587dd41894afd38b
Joao <joaovictorcordeiro00@gmail.com>
2024-02-27 11:54:11 -0300

    Implementando tabela das pessoas cadastradas

--- src/pages/people/PeopleListing.tsx
+++ src/pages/people/PeopleListing.tsx
@@ -4,8 +4,10 @@ import { ListingTools } from '../../shared/components';
 import { useSearchParams } from 'react-router-dom';
 import { IPeopleListing, PeopleService } from '../../shared/services/api/people/PeopleService';
 import { UseDebounce } from '../../shared/hooks';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 export const PeopleListing: React.FC = () => {
+  const [ isLoading, setIsLoading ] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
   const { debounce } = UseDebounce(); //ele vai executar o UseDebounce
   const [ rows, setRows ] = useState<IPeopleListing[]>([]);
@@ -16,14 +18,21 @@ export const PeopleListing: React.FC = () => {
   }, [searchParams]);
 
   useEffect(() => {
+    setIsLoading(true);
 
     debounce(() => {
       PeopleService.getAll(1, search).then((resultado) => {
+
+        setIsLoading(false);
+
         if (resultado instanceof Error) {
           alert(resultado.message);
           return;
         }
         console.log(resultado);
+
+        setTotalCount(resultado.totalCount);
+        setRows(resultado.data);
       });
     }); //este corpo executa a consulta ao banco de dados
 
@@ -40,6 +49,35 @@ export const PeopleListing: React.FC = () => {
           searchText={search}
         />
       }
-    ></PageBaseLayout>
+    >
+
+      <TableContainer 
+        component={Paper} 
+        variant='outlined' 
+        sx={{ m: 1, width:'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><strong>Full Name</strong></TableCell>
+              <TableCell><strong>E-Mail</strong></TableCell>
+              <TableCell><strong>Actions</strong></TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>{row.fullName}</TableCell>
+                <TableCell>{row.email}</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+        </Table>
+      </TableContainer>
+
+
+    </PageBaseLayout>
   );
 };
