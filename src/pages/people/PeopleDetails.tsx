e97d911fd4b5691bd28b638f315361e1c63b6d64
Joao <joaovictorcordeiro00@gmail.com>
2024-03-14 11:50:32 -0300

    Implementando o formulário do unform e editando o saveHandle

--- src/pages/people/PeopleDetails.tsx
+++ src/pages/people/PeopleDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { PageBaseLayout } from '../../shared/layouts';
 import { DetailTools } from '../../shared/components';
@@ -6,13 +6,16 @@ import { PeopleService } from '../../shared/services/api/people/PeopleService';
 import { LinearProgress } from '@mui/material';
 import { Form } from '@unform/web';
 import { VTextField } from '../../shared/forms';
+import { FormHandles } from '@unform/core';
 
-// type Test = 'id' | 'name' | 'description'
-
-// const testing: Test = 'id';
-// console.log(testing);
+interface IFormData {
+  fullName: string,
+  email: string,
+  cityId: number
+}
 
 export const PeopleDetails: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigate = useNavigate(); 
   const {id = 'new'} = useParams<'id'>();
   const [ isLoading, setIsLoading ] = useState(false);
@@ -39,8 +42,8 @@ export const PeopleDetails: React.FC = () => {
   }, [id]);
 
 
-  const saveHandle = () => {
-    console.log('Save');
+  const saveHandle = (data: IFormData) => {
+    console.log(data);
   };
 
   const deleteHandle = (id: number, fullName: string) => {
@@ -71,8 +74,8 @@ export const PeopleDetails: React.FC = () => {
           whenNewIsPressed={() => navigate('/people/details/new')}
           whenDeleteIsPressed={() => deleteHandle(Number(id), name) }
           whenBackIsPressed={() => navigate('/people')}
-          whenSaveIsPressed={ saveHandle }
-          whenSaveAndExitIsPressed={ saveHandle }
+          whenSaveIsPressed={ () => formRef.current?.submitForm() } //esse submitForm permite que a gente consiga fazer o submit do formulário que está fora do nosso Form
+          whenSaveAndExitIsPressed={ () => formRef.current?.submitForm() }
         />
       }
     >
@@ -80,11 +83,29 @@ export const PeopleDetails: React.FC = () => {
         <LinearProgress variant='indeterminate'/>
       )}
 
-      <Form onSubmit={(data) => console.log(data)} placeholder={undefined}>
+      {/* passamos o saveHandle dentro do onSubmit, pois quando alteramos o onSubmit do formulário, queremos que os dados que o unform vai estar entregando para nós, vão para o saveHandle.
+      Dessa forma, podemos tratar toda a parte de salvar no banco de dados e fazer outras operações */}
+      <Form ref={formRef} onSubmit={saveHandle} placeholder={undefined}>
+
+        {/* nos permite pegar a referênca do nosso componente de formulário e deixar ela armazenada dentro desse formRef. Com isso, conseguimos dar um submit manual
+        do formulário através do nosso componente da listing_tools */}
         
         <VTextField name='fullName'/>
-        <VTextField name='cityId'/>
         <VTextField name='email'/>
+        <VTextField name='cityId'/>
+
+        {/* {[1, 2].map((_, index) => (
+          <>
+            <VTextField name={`address[${index}].street`}/>
+            <VTextField name={`address[${index}].number`}/>
+            <VTextField name={`address[${index}].state`}/>
+            <VTextField name={`address[${index}].city`}/>
+            <VTextField name={`address[${index}].country`}/>
+          
+          </>
+        ))} */}
+        
+
 
         <button type='submit'>Submit</button>
       </Form>
