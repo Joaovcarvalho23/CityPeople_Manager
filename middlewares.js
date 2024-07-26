658d5a0ae1497625f1644f754ea37c4bddddb178
joao victor <joaovcgcordeiro@gmail.com>
2024-07-26 09:45:48 -0300

    desabilitando cache do navegador

--- middlewares.js
+++ middlewares.js
@@ -1,19 +1,18 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./mock/database.json');
+const router = jsonServer.router('mock/database.json');
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Cache-Control', 'no-store');
+  res.header('Pragma', 'no-cache');
+  res.header('Expires', '0');
   next();
 });
 
+server.use(middlewares);
 server.use(router);
 
 server.listen(3333, () => {
-  console.log('JSON Server is running on port 3333');
+  console.log('JSON Server está sendo executado');
 });
